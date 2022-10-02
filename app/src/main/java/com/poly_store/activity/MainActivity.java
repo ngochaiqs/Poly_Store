12b553fe0470317be8fe4ed9906eafe5e88eeeea
legon <legonvhien9.3@gmail.com>
2022-10-02 17:08:15 +0700

    Gonsadboiz tess

--- app/src/main/java/com/poly_store/activity/MainActivity.java
+++ app/src/main/java/com/poly_store/activity/MainActivity.java
@@ -1,16 +1,75 @@
 package com.poly_store.activity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.poly_store.R;
+import com.poly_store.adapter.LoaiSPAdapter;
+import com.poly_store.model.LoaiSP;
+import com.poly_store.retrofit.ApiBanHang;
+import com.poly_store.retrofit.RetrofitClient;
+import com.poly_store.utils.Utils;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    ApiBanHang apiBanHang;
+    List<LoaiSP> mangloaisp;
+    LoaiSPAdapter loaiSPAdapter;
+    ListView listViewManHinhChinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+
+        if(isConnected(this)){
+            Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
+            ActionViewFlipper();
+            getLoaiSanPham();
+        }else {
+            Toast.makeText(getApplicationContext(), "khong co internet, vui long ket noi", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    private void getLoaiSanPham() {
+        compositeDisposable.add(apiBanHang.getLoaiSp()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        loaiSPModel -> {
+                            if (loaiSPModel.isSuccess()){
+                                mangloaisp = loaiSPModel.getResult();
+                                loaiSPAdapter = new LoaiSPAdapter(getApplicationContext(),mangloaisp);
+                                listViewManHinhChinh.setAdapter(loaiSPAdapter);
+                            }
+                        }
+                ));
+    }
+
+    public boolean isConnected (Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager)  context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if((wifi != null && wifi.isConnected()) || (mobile != null && mobile.isConnected())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
 }
--- app/src/main/java/com/poly_store/adapter/LoaiSPAdapter.java
+++ app/src/main/java/com/poly_store/adapter/LoaiSPAdapter.java
@@ -1,4 +1,10 @@
 package com.poly_store.adapter;
 
 public class LoaiSPAdapter {
+
+
+    //else{ viewHolder = (ViewHolder). view.getTag();
+    //}
+    //viewHolder.texttensp.setText(array.get(i).....
+    //Glide.with(context).....
 }
