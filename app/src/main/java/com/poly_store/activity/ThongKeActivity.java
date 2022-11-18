f2289166c2e9899862c2fa1f382e6ad21cd96e7c
legon <legonvhien9.3@gmail.com>
2022-11-18 17:36:17 +0700

    Thong ke SP DT

--- app/src/main/java/com/poly_store/activity/ThongKeActivity.java
+++ app/src/main/java/com/poly_store/activity/ThongKeActivity.java
@@ -1,103 +1,47 @@
 package com.poly_store.activity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.poly_store.R;
-import com.poly_store.retrofit.ApiBanHang;
-import com.poly_store.retrofit.RetrofitClient;
-import com.poly_store.utils.Utils;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
+import com.poly_store.activity.ui.main.SectionsPagerAdapter;
+import com.poly_store.databinding.ActivityThongKeBinding;
 
 public class ThongKeActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    PieChart pieChart;
-    CompositeDisposable compositeDisposable = new CompositeDisposable();
-    ApiBanHang apiBanHang;
+
+    private ActivityThongKeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_thong_ke);
-        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
-        initview();
-        ActionToolBar();
-        getdataChart();
 
-    }
+        binding = ActivityThongKeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-    private void getdataChart() {
-        List<PieEntry> listdata = new ArrayList<>();
-        compositeDisposable.add(apiBanHang.getThongKe()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        thongKeModel -> {
-                            if (thongKeModel.isSuccess()) {
-                                for (int i = 0; i < thongKeModel.getResult().size(); i++) {
-                                    String tensp = thongKeModel.getResult().get(i).getTenSP();
-                                    int tong = thongKeModel.getResult().get(i).getTong();
-                                    listdata.add(new PieEntry(tong, tensp));
-                                }
-                                PieDataSet pieDataSet = new PieDataSet(listdata, "Thống kê");
-                                PieData data = new PieData();
-                                data.setDataSet(pieDataSet);
-                                pieDataSet.setValueTextColor(Color.BLACK);
-                                data.setValueTextSize(12f);
-                                data.setValueFormatter(new PercentFormatter(pieChart));
-                                pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
 
-                                pieChart.setCenterTextColor(Color.BLACK);
-                                pieChart.setData(data);
-                                pieChart.animateXY(2000, 2000);
-                                pieChart.setUsePercentValues(true);
-                                pieChart.getDescription().setEnabled(false);
-                                pieChart.invalidate();
-
-                            }
-                        },
-                        throwable -> {
-                            Log.d("logg", throwable.getMessage());
-                        }
-                ));
-    }
-
-    private void initview() {
-        toolbar = findViewById(R.id.toolbarThongKe);
-        pieChart = findViewById(R.id.piechartThongKe);
-    }
-
-    private void ActionToolBar() {
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                finish();
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    protected void onDestroy() {
-        compositeDisposable.clear();
-        super.onDestroy();
     }
 }
