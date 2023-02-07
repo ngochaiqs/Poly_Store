a3723233ee8ac6105c557c1985e024f7905d670b
Nguyen <nguyentnpd05054@fpt.edu.vn>
2023-02-07 22:49:20 +0700

    nguyen123

---
+++ app/src/main/java/com/poly_store/activity/Store.java
@@ -0,0 +1,14 @@
+package com.poly_store.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Store extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
