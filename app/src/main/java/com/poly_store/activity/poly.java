fc46d8022cdffb775ce2f315193b610bc29fcb41
Nguyen <nguyentnpd05054@fpt.edu.vn>
2023-02-07 22:48:12 +0700

    nguyen12

---
+++ app/src/main/java/com/poly_store/activity/poly.java
@@ -0,0 +1,16 @@
+package com.poly_store.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.poly_store.R;
+
+public class poly extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poly);
+    }
+}
